2c20496612c6f0e23a092cfa1e387c6157623999
JeremyEvrard <jevrard_1@hotmail.com>
2014-09-27 09:25:57 +0200

    corr bug

--- page-a-propos.php
+++ page-a-propos.php
@@ -1,23 +1,22 @@
 <?php get_header(); ?>
     <section class="about">
+	<?php 
+        // The Query
+        $the_query = new WP_Query( 'pagename=a-propos' );
 
-         <?php 
-            // The Query
-            $the_query = new WP_Query( 'pagename=a-propos' );
-
-            // The Loop
-            if ( $the_query->have_posts() ) {
-                while ( $the_query->have_posts() ) {
-                    $the_query->the_post();
-                    echo the_content();
-                }
-            } else {
-                // no posts found
+        // The Loop
+        if ( $the_query->have_posts() ) {
+            while ( $the_query->have_posts() ) {
+                $the_query->the_post();
+                echo the_content();
             }
-            /* Restore original Post Data */
-            wp_reset_postdata();
+        } else {
+            // no posts found
+        }
+        /* Restore original Post Data */
+        wp_reset_postdata();
+    ?>
 
 
-          ?>
     </section>
 <?php get_footer(); ?>
